ceb7ccf5610016574511bf7535833525d0d792db
Pisarev <63995107+pisarewk@users.noreply.github.com>
2026-06-22 18:27:10 +0300

    3

--- app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.models.user import User, RoleEnum
-from app.models.candidate import Candidate
-from app.models.employer import Employer
-from app.models.vacancy import Vacancy, VacancyStatusEnum
-from app.models.application import Application, StatusEnum
-from app.models.chat import Chat, Message
-from app.models.resume import Resume
-
-__all__ = ["User", "RoleEnum", "Candidate", "Employer", "Vacancy", "VacancyStatusEnum", "Application", "StatusEnum", "Chat", "Message", "Resume"]
